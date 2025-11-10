657da8b61a63885da52ca743f191fec601788802
arieeshkol1 <ariel.eshkol@gmail.com>
2025-11-11 00:19:42 +0200

    Update test_cdk_chatbot_api_stack.py

--- tests/unit/cdk/test_cdk_chatbot_api_stack.py
+++ tests/unit/cdk/test_cdk_chatbot_api_stack.py
@@ -45,22 +45,6 @@ template: assertions.Template = assertions.Template.from_stack(stack)
 def test_app_synthesize_ok():
     app.synth()
 
-
-#def test_dynamodb_table_created():
-#    match = template.find_resources(
-#        type="AWS::DynamoDB::Table",
-#    )
- #   assert match, "No DynamoDB tables were synthesized"
-#
-#    table_names = {
-#        resource["Properties"].get("TableName") for resource in match.values()
- #   }
-
- #   assert (
- #       "aws-whatsapp-poc-test1" in table_names
- #   ), "Primary chatbot conversations table is missing"
-
-
 def test_lambda_function_created():
     match = template.find_resources(
         type="AWS::Lambda::Function",
